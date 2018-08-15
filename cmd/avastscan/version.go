986bf5985ec46e784d0f3e91f349d0bb028aebeb
Andrew Colin Kissa <andrew@topdog.za.net>
2018-08-15 11:52:35 +0200

    FET: Add version variables

--- cmd/avastscan/version.go
+++ cmd/avastscan/version.go
@@ -8,3 +8,18 @@ Package main Golang Avast client
 Avast - Golang Avast cmdline client
 */
 package main
+
+// GitCommit is the git commit that was compiled.
+// This will be filled in by the compiler.
+var GitCommit string
+
+// Version is the main version number that is being run at the moment.
+const Version = "0.0.1"
+
+// VersionPrerelease is a pre-release marker for the version.
+// If this is "" (empty string) then it means that it is a final release.
+// Otherwise, this is a pre-release such as "dev" (in development)
+var VersionPrerelease = ""
+
+// BuildDate is the build date
+var BuildDate = ""
