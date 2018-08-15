33c29bac3c4354de5e62d05ad2a0dd6d180e2935
Andrew Colin Kissa <andrew@topdog.za.net>
2018-08-15 17:05:22 +0200

    FET: Implemented various methods

--- avast.go
+++ avast.go
@@ -11,8 +11,10 @@ package avast
 
 import (
 	"fmt"
+	"net"
 	"net/textproto"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -41,6 +43,230 @@ const (
 	// Quit sends the QUIT request
 	Quit
 )
+
+const (
+	// Mime represents mime option
+	Mime PackOption = iota + 1
+	// Zip represents zip pack option
+	Zip
+	// Arj represents arj pack option
+	Arj
+	// Rar represents rar pack option
+	Rar
+	// Cab represents cab pack option
+	Cab
+	// Tar represents tar pack option
+	Tar
+	// Gz represents gz pack option
+	Gz
+	// Bzip2 represents bzip2 pack option
+	Bzip2
+	// Ace represents ace pack option
+	Ace
+	// Arc represents arc pack option
+	Arc
+	// Zoo represents zoo pack option
+	Zoo
+	// Lharc represents lharc pack option
+	Lharc
+	// Chm represents chm pack option
+	Chm
+	// Cpio represents cpio pack option
+	Cpio
+	// Rpm represents rpm pack option
+	Rpm
+	// Szip represents 7zip pack option
+	Szip
+	// Iso represents iso pack option
+	Iso
+	// Tnef represents tnef pack option
+	Tnef
+	// Dbx represents dbx pack option
+	Dbx
+	// Sys represents sys pack option
+	Sys
+	// Ole represents ole pack option
+	Ole
+	// Exec represents exec pack option
+	Exec
+	// WinExec represents winexec pack option
+	WinExec
+	// Install represents install option
+	Install
+	// Dmg represents dmg pack option
+	Dmg
+)
+
+const (
+	// FullFiles is fullfiles
+	FullFiles Flag = iota + 1
+	// AllFiles is allfiles
+	AllFiles
+	// ScanDevices is scandevices
+	ScanDevices
+)
+
+const (
+	// Worm represents worm
+	Worm SensiOption = iota + 1
+	// Trojan represents trojan
+	Trojan
+	// Adware represents adware
+	Adware
+	// Spyware represents spyware
+	Spyware
+	// Dropper represents dropper
+	Dropper
+	// Kit represents kit
+	Kit
+	// Joke represents joke
+	Joke
+	// Dangerous represents dangerous
+	Dangerous
+	// Dialer represents dialer
+	Dialer
+	// Rootkit represents rootkit
+	Rootkit
+	// Exploit represents exploit
+	Exploit
+	// Pup represents pup
+	Pup
+	// Suspicious represents suspicious
+	Suspicious
+	// Pube represents pube
+	Pube
+)
+
+// SensiOption represents Avast Sensitivity options
+type SensiOption int
+
+func (so SensiOption) String() (s string) {
+	n := [...]string{
+		"",
+		"worm",
+		"trojan",
+		"adware",
+		"spyware",
+		"dropper",
+		"kit",
+		"joke",
+		"dangerous",
+		"dialer",
+		"rootkit",
+		"exploit",
+		"pup",
+		"suspicious",
+		"pube",
+	}
+	if so < Worm || so > Pube {
+		s = ""
+		return
+	}
+
+	s = n[so]
+
+	return
+}
+
+// Enable returns enabled option string
+func (so SensiOption) Enable() (s string) {
+	s = fmt.Sprintf("+%s", so)
+
+	return
+}
+
+// Disable returns disabled option string
+func (so SensiOption) Disable() (s string) {
+	s = fmt.Sprintf("-%s", so)
+
+	return
+}
+
+// A Flag represents Avast flags
+type Flag int
+
+func (f Flag) String() (s string) {
+	n := [...]string{
+		"",
+		"fullfiles",
+		"allfiles",
+		"scandevices",
+	}
+	if f < FullFiles || f > ScanDevices {
+		s = ""
+		return
+	}
+	s = n[f]
+	return
+}
+
+// Enable returns enabled option string
+func (f Flag) Enable() (s string) {
+	s = fmt.Sprintf("+%s", f)
+
+	return
+}
+
+// Disable returns disabled option string
+func (f Flag) Disable() (s string) {
+	s = fmt.Sprintf("-%s", f)
+
+	return
+}
+
+// A PackOption represents Avast PACK options
+type PackOption int
+
+func (p PackOption) String() (s string) {
+	n := [...]string{
+		"",
+		"mime",
+		"zip",
+		"arj",
+		"rar",
+		"cab",
+		"tar",
+		"gz",
+		"bzip2",
+		"ace",
+		"arc",
+		"zoo",
+		"lharc",
+		"chm",
+		"cpio",
+		"rpm",
+		"7zip",
+		"iso",
+		"tnef",
+		"dbx",
+		"sys",
+		"ole",
+		"exec",
+		"winexec",
+		"install",
+		"dmg",
+	}
+	if p < Mime || p > Dmg {
+		s = ""
+		return
+	}
+	s = n[p]
+	return
+}
+
+// Enable returns enabled option string
+func (p PackOption) Enable() (s string) {
+	s = fmt.Sprintf("+%s", p)
+
+	return
+}
+
+// Disable returns disabled option string
+func (p PackOption) Disable() (s string) {
+	s = fmt.Sprintf("-%s", p)
+
+	return
+}
 
 // A Command represents an Avast Command
 type Command int
@@ -118,36 +344,121 @@ func (c *Client) Scan(p string) (r []*Response, err error) {
 
 // Vps returns the virus definitions (VPS) version
 func (c *Client) Vps() (v int, err error) {
+	var l int
+	var s string
+
+	s, err = c.basicCmd(Vps, "")
+	if err != nil {
+		return
+	}
+
+	l = len(s)
+	if l < 4 || s[0] != 'V' && s[1] != 'P' && s[2] != 'S' && s[3] != ' ' {
+		err = fmt.Errorf("Invalid Server Response: %s", s)
+		return
+	}
+
+	if v, err = strconv.Atoi(s[4:]); err != nil {
+		err = fmt.Errorf("Invalid Server Response: %s", s)
+		return
+	}
+
 	return
 }
 
 // GetPack returns packer options
-func (c *Client) GetPack() (err error) {
+func (c *Client) GetPack() (p string, err error) {
+	var l int
+	var s string
+	s, err = c.basicCmd(Pack, "")
+	if err != nil {
+		return
+	}
+
+	l = len(s)
+	if l < 5 || s[0] != 'P' && s[3] != 'K' && s[4] != ' ' {
+		err = fmt.Errorf("Invalid Server Response: %s", s)
+		return
+	}
+
+	p = s[5:]
 	return
 }
 
 // SetPack sets packer options
-func (c *Client) SetPack() (err error) {
+func (c *Client) SetPack(o PackOption, v bool) (err error) {
+	var s string
+	if v {
+		s = o.Enable()
+	} else {
+		s = o.Disable()
+	}
+
+	_, err = c.basicCmd(Pack, s)
 	return
 }
 
 // GetFlags returns scan flags
-func (c *Client) GetFlags() (err error) {
+func (c *Client) GetFlags() (f string, err error) {
+	var l int
+	var s string
+	s, err = c.basicCmd(Flags, "")
+	if err != nil {
+		return
+	}
+
+	l = len(s)
+	if l < 6 || s[0] != 'F' && s[4] != 'K' && s[5] != ' ' {
+		err = fmt.Errorf("Invalid Server Response: %s", s)
+		return
+	}
+
+	f = s[6:]
 	return
 }
 
 // SetFlags sets scan flags
-func (c *Client) SetFlags() (err error) {
+func (c *Client) SetFlags(o Flag, v bool) (err error) {
+	var s string
+	if v {
+		s = o.Enable()
+	} else {
+		s = o.Disable()
+	}
+
+	_, err = c.basicCmd(Flags, s)
 	return
 }
 
-// GetSensitivity returns scan sensitivity
-func (c *Client) GetSensitivity() (err error) {
+// GetSensitivity returns scan sensitivity options
+func (c *Client) GetSensitivity() (f string, err error) {
+	var l int
+	var s string
+	s, err = c.basicCmd(Sensitivity, "")
+	if err != nil {
+		return
+	}
+
+	l = len(s)
+	if l < 12 || s[0] != 'S' && s[10] != 'Y' && s[11] != ' ' {
+		err = fmt.Errorf("Invalid Server Response: %s", s)
+		return
+	}
+
+	f = s[11:]
 	return
 }
 
 // SetSensitivity sets scan sensitivity
-func (c *Client) SetSensitivity() (err error) {
+func (c *Client) SetSensitivity(o SensiOption, v bool) (err error) {
+	var s string
+	if v {
+		s = o.Enable()
+	} else {
+		s = o.Disable()
+	}
+
+	_, err = c.basicCmd(Sensitivity, s)
 	return
 }
 
@@ -161,8 +472,73 @@ func (c *Client) CheckURL(u string) (r bool, err error) {
 	return
 }
 
+// Close closes the server connection
+func (c *Client) Close() (err error) {
+	_, err = c.basicCmd(Quit, "")
+
+	c.tc.Close()
+
+	return
+}
+
+func (c *Client) dial() (conn net.Conn, err error) {
+	d := &net.Dialer{}
+
+	if c.connTimeout > 0 {
+		d.Timeout = c.connTimeout
+	}
+
+	for i := 0; i <= c.connRetries; i++ {
+		conn, err = d.Dial("unix", c.address)
+		if e, ok := err.(net.Error); ok && e.Timeout() {
+			time.Sleep(c.connSleep)
+			continue
+		}
+		break
+	}
+	return
+}
+
+func (c *Client) basicCmd(cmd Command, o string) (r string, err error) {
+	var id uint
+	if o == "" {
+		id, err = c.tc.Cmd("%s", cmd)
+	} else {
+		id, err = c.tc.Cmd("%s %s", cmd, o)
+	}
+
+	if err != nil {
+		return
+	}
+
+	c.tc.StartResponse(id)
+	defer c.tc.EndResponse(id)
+
+	if cmd == Quit {
+		return
+	}
+
+	// Read Opening response
+	if _, _, err = c.tc.ReadCodeLine(210); err != nil {
+		return
+	}
+
+	// Read actual response
+	if r, err = c.tc.ReadLine(); err != nil {
+		return
+	}
+
+	// Read Closing response
+	if _, _, err = c.tc.ReadCodeLine(200); err != nil {
+		return
+	}
+
+	return
+}
+
 // NewClient creates and returns a new instance of Client
 func NewClient(address string) (c *Client, err error) {
+	var conn net.Conn
 	if address == "" {
 		address = avastSock
 	}
@@ -176,6 +552,21 @@ func NewClient(address string) (c *Client, err error) {
 		address:     address,
 		connTimeout: defaultTimeout,
 		connSleep:   defaultSleep,
+	}
+
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	if c.tc == nil {
+		if conn, err = c.dial(); err != nil {
+			return
+		}
+
+		c.tc = textproto.NewConn(conn)
+	}
+
+	if _, _, err = c.tc.ReadCodeLine(220); err != nil {
+		return
 	}
 
 	return
